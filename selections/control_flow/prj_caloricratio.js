cde0a68c02fef5dda543e1228d5723f9cfea8631
Benito Gonzalez <benitogonzalezh@gmail.com>
2021-06-25 16:48:47 -0400

    imdb project

---
+++ selections/control_flow/prj_caloricratio.js
@@ -0,0 +1,65 @@
+const dryFruitData = [
+  {
+    name: 'Almonds',
+    carbs: 15,
+    fats: 72,
+    proteins: 13
+  },
+  {
+    name: 'Raisins',
+    carbs: 96,
+    fats: 1,
+    proteins: 3
+  },
+  {
+    name: 'Cashews',
+    carbs: 23,
+    fats: 66,
+    proteins: 11
+  },
+  {
+    name: 'Walnuts',
+    carbs: 9,
+    fats: 83,
+    proteins: 8
+  },
+  {
+    name: 'Apricots',
+    carbs: 93,
+    fats: 2,
+    proteins: 5
+  },
+  {
+    name: 'Pistachios',
+    carbs: 20,
+    fats: 67,
+    proteins: 13
+  },
+  {
+    name: 'Figs',
+    carbs: 94,
+    fats: 3,
+    proteins: 3
+  },
+  {
+    name: 'Dates',
+    carbs: 98,
+    fats: 0,
+    proteins: 2
+  },
+  {
+    name: 'Prunes',
+    carbs: 96,
+    fats: 1,
+    proteins: 3
+  },
+  {
+    name: 'Hazelnuts',
+    carbs: 11,
+    fats: 81,
+    proteins: 8
+  },
+];
+
+const svgWidth = d3.select('#chart').node().clientWidth;
+console.log(svgWidth);
